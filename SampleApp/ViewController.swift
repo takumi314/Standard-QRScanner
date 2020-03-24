314254d2c1b6a6fe94489ac518178032e997a90d
takumi314 <isospin_symmetry@me.com>
2020-03-25 01:15:34 +0900

    Create SampleApp project

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by NishiokaKohei on 25/03/2020.
+//  Copyright © 2020 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
