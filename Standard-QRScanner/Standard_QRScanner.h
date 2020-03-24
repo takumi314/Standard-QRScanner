b8efc1bc858293912155bed6eecbfccf902e846a
takumi314 <isospin_symmetry@me.com>
2020-03-25 01:07:00 +0900

    Initial commit

---
+++ Standard-QRScanner/Standard_QRScanner.h
@@ -0,0 +1,19 @@
+//
+//  Standard_QRScanner.h
+//  Standard-QRScanner
+//
+//  Created by NishiokaKohei on 25/03/2020.
+//  Copyright © 2020 Takumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Standard_QRScanner.
+FOUNDATION_EXPORT double Standard_QRScannerVersionNumber;
+
+//! Project version string for Standard_QRScanner.
+FOUNDATION_EXPORT const unsigned char Standard_QRScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Standard_QRScanner/PublicHeader.h>
+
+
